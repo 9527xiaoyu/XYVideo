72b800f8b1ccc6be4fc209bdbe3a7ed26f6b0238
9527xiaoyu <1446899815@qq.com>
2018-11-28 11:42:20 +0800

    add: firstUpdate

---
+++ XYVideo-ios/XYVideo-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYVideo-ios
+//
+//  Created by 4AM_Xiao on 2018/11/28.
+//  Copyright © 2018年 com.ywart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYVideo-ios/XYVideo-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYVideo-ios
+//
+//  Created by 4AM_Xiao on 2018/11/28.
+//  Copyright © 2018年 com.ywart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
